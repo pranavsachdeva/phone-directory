58915dbd2cd206916e33c9b5c4868ca0e2bad4a5
pranavsachdeva <pranavsachdeva2000@gmail.com>
2019-12-04 22:26:54 -0800

    Added 'AddSubscriber' component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
+import AddSusbscriber from './AddSubscriber';
+import './common/common.css';
+ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
