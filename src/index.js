08ecc6e1bef2dccd008cc4f88888f09fcd71f2d8
pranavsachdeva <pranavsachdeva2000@gmail.com>
2019-12-08 21:22:17 -0800

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import PhoneDirectory from './PhoneDirectory';
+ReactDOM.render(<PhoneDirectory />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
